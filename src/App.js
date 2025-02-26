b52a5be24adf7d89612ef3dcfce7c84e40b4cf46
jhb0302 <gd8546@naver.com>
2025-02-26 09:04:32 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            EKSEKS DEMO Blog version 1
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
